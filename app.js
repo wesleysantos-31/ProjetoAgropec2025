594c5031cc7271954058628f12701bb87e311cee
Isabella Pompeo <162395260+Isabella-Pompeo@users.noreply.github.com>
2025-06-20 13:49:02 -0300

    carrosel funcionando, alteracao no banner

--- app.js
+++ app.js
@@ -1107,4 +1107,5 @@ let carouselIndex = 0;
     document.getElementById("carousel-images").style.transform = `translateX(-${carouselIndex * 100}%)`;
   }
 
- 
+  // rotação automática
+  setInterval(() => moveCarousel(1), 5000);
